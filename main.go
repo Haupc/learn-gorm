9b8b30db988e149bca9b5226ae8fb8d064eeff12
haupc <haupc.uet@gmail.com>
2021-01-07 21:17:27 +0700

    bug

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ import (
 
 func main() {
 	dbConfig.InitConnection()
-	service.GenRoleInsertStatement(35, 36)
+	// service.GenRoleInsertStatement(35, 36)
 	service.GenPermissionInsertStatement(35, 36)
-	service.GenAPIInsertStatement(35, 36)
+	// service.GenAPIInsertStatement(35, 36)
 	// fmt.Println(service.GetStatement())
 	fmt.Println(repository.Permissions)
 }
